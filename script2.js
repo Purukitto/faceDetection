d2cc6fa3e1e842cfd879063701c5bbb7aecc0082
Purukitto <purukitto@gmail.com>
2020-10-21 12:34:13 +0530

    Deleted my model

--- script2.js
+++ script2.js
@@ -53,7 +53,7 @@ video.addEventListener('play', async() => {
 
 
 function loadLabeledImages() {
-    const labels = ['Pulkit Singh']
+    const labels = ['Benedict Cumberbatch', 'Emma Watson', 'Martin Freeman', 'Pulkit Singh']
     return Promise.all(
         labels.map(async label => {
             const descriptions = []
